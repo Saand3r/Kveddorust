b2014bc2e9e440c2becfb62f48d9de2d8609ac15
Saand3r <sander-js@hotmail.com>
2022-05-26 21:47:42 +0200

    Finishing up 4 today

--- public/js/store.js
+++ public/js/store.js
@@ -1,12 +1,4 @@
-
-fetch('http://localhost:3000/api/steamuser')
-    .then(res => {
-        console.log("fetch OK")
-        //const nameP = document.getElementById('nameViewer').innerHTML = res[0]
-        //const idP = document.getElementById('steamId').innerHTML = res[1]
-    })
-    /* .then(res => {
-        const nameP = document.getElementById('nameViewer').innerHTML = res[0]
-        const idP = document.getElementById('steamId').innerHTML = res[1]
-    }) */
-    .catch(error => console.log(error))
+var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
+var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
+    return new bootstrap.Dropdown(dropdownToggleEl)
+})
