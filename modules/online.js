e5613830b2990a83ce2a1d28058a0f5a2a847c1c
Saand3r <sander-js@hotmail.com>
2022-08-25 18:54:42 +0200

    fix?

--- modules/online.js
+++ modules/online.js
@@ -3,7 +3,7 @@ async function onlinePlay() {
   Gamedig.query({
     type: 'rust',
     host: 'oslo14.spillvert.no',
-    port: 28215,
+    port: 28216,
     requestRules: true
 }).then((state) => {
     const response = state;
@@ -15,4 +15,4 @@ async function onlinePlay() {
 }).catch((error) => {
     console.log("Sumthing went wrong" + error);
 })};
-/* module.exports = onlinePlay(); */
+module.exports = onlinePlay();
